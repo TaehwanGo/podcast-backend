68689ca361ce5928d296d988aa55ff04fd6e857c
TaehwanGo <gth1123@naver.com>
2021-01-22 16:06:06 +0900

    /

--- src/podcast/podcasts.service.ts
+++ src/podcast/podcasts.service.ts
@@ -1,36 +1,36 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 import {
   CreateEpisodeInput,
-  CreateEpisodeOutput
-} from "./dtos/create-episode.dto";
+  CreateEpisodeOutput,
+} from './dtos/create-episode.dto';
 import {
   CreatePodcastInput,
-  CreatePodcastOutput
-} from "./dtos/create-podcast.dto";
-import { UpdateEpisodeInput } from "./dtos/update-episode.dto";
-import { UpdatePodcastInput } from "./dtos/update-podcast.dto";
-import { Episode } from "./entities/episode.entity";
-import { Review } from "./entities/review.entity";
-import { Podcast } from "./entities/podcast.entity";
-import { CoreOutput } from "./dtos/output.dto";
+  CreatePodcastOutput,
+} from './dtos/create-podcast.dto';
+import { UpdateEpisodeInput } from './dtos/update-episode.dto';
+import { UpdatePodcastInput } from './dtos/update-podcast.dto';
+import { Episode } from './entities/episode.entity';
+import { Review } from './entities/review.entity';
+import { Podcast } from './entities/podcast.entity';
+import { CoreOutput } from './dtos/output.dto';
 import {
   PodcastOutput,
   EpisodesOutput,
   EpisodesSearchInput,
   GetAllPodcastsOutput,
-  GetEpisodeOutput
-} from "./dtos/podcast.dto";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository, Raw, Like } from "typeorm";
+  GetEpisodeOutput,
+} from './dtos/podcast.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, Raw, Like } from 'typeorm';
 import {
   SearchPodcastsInput,
-  SearchPodcastsOutput
-} from "./dtos/search-podcasts.dto";
+  SearchPodcastsOutput,
+} from './dtos/search-podcasts.dto';
 import {
   CreateReviewInput,
-  CreateReviewOutput
-} from "./dtos/create-review.dto";
-import { User } from "src/users/entities/user.entity";
+  CreateReviewOutput,
+} from './dtos/create-review.dto';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class PodcastsService {
@@ -40,12 +40,13 @@ export class PodcastsService {
     @InjectRepository(Episode)
     private readonly episodeRepository: Repository<Episode>,
     @InjectRepository(Review)
-    private readonly reviewRepository: Repository<Review>
+    private readonly reviewRepository: Repository<Review>,
   ) {}
 
   private readonly InternalServerErrorOutput = {
+    //
     ok: false,
-    error: "Internal server error occurred."
+    error: 'Internal server error occurred.',
   };
 
   async getAllPodcasts(): Promise<GetAllPodcastsOutput> {
@@ -53,7 +54,7 @@ export class PodcastsService {
       const podcasts = await this.podcastRepository.find();
       return {
         ok: true,
-        podcasts
+        podcasts,
       };
     } catch (e) {
       return this.InternalServerErrorOutput;
@@ -62,7 +63,7 @@ export class PodcastsService {
 
   async createPodcast(
     creator: User,
-    { title, category }: CreatePodcastInput
+    { title, category }: CreatePodcastInput,
   ): Promise<CreatePodcastOutput> {
     try {
       const newPodcast = this.podcastRepository.create({ title, category });
@@ -70,7 +71,7 @@ export class PodcastsService {
       const { id } = await this.podcastRepository.save(newPodcast);
       return {
         ok: true,
-        id
+        id,
       };
     } catch (e) {
       return this.InternalServerErrorOutput;
@@ -81,17 +82,17 @@ export class PodcastsService {
     try {
       const podcast = await this.podcastRepository.findOne(
         { id },
-        { relations: ["episodes", "creator", "reviews"] }
+        { relations: ['episodes', 'creator', 'reviews'] },
       );
       if (!podcast) {
         return {
           ok: false,
-          error: `Podcast with id ${id} not found`
+          error: `Podcast with id ${id} not found`,
         };
       }
       return {
         ok: true,
-        podcast
+        podcast,
       };
     } catch (e) {
       return this.InternalServerErrorOutput;
@@ -105,7 +106,7 @@ export class PodcastsService {
         return { ok, error };
       }
       if (podcast.creator.id !== user.id) {
-        return { ok: false, error: "Not authorized" };
+        return { ok: false, error: 'Not authorized' };
       }
       await this.podcastRepository.delete({ id });
       return { ok };
@@ -116,7 +117,7 @@ export class PodcastsService {
 
   async updatePodcast(
     user: User,
-    { id, payload }: UpdatePodcastInput
+    { id, payload }: UpdatePodcastInput,
   ): Promise<CoreOutput> {
     try {
       const { ok, error, podcast } = await this.getPodcast(id);
@@ -124,7 +125,7 @@ export class PodcastsService {
         return { ok, error };
       }
       if (podcast.creator.id !== user.id) {
-        return { ok: false, error: "Not authorized" };
+        return { ok: false, error: 'Not authorized' };
       }
       if (
         payload.rating !== null &&
@@ -132,7 +133,7 @@ export class PodcastsService {
       ) {
         return {
           ok: false,
-          error: "Rating must be between 1 and 5."
+          error: 'Rating must be between 1 and 5.',
         };
       } else {
         const updatedPodcast: Podcast = { ...podcast, ...payload };
@@ -146,23 +147,23 @@ export class PodcastsService {
 
   async searchPodcasts({
     titleQuery,
-    page
+    page,
   }: SearchPodcastsInput): Promise<SearchPodcastsOutput> {
     try {
       const [podcasts, totalCount] = await this.podcastRepository.findAndCount({
         // where: { title: Raw((title) => `${title} LIKE ${titleQuery}`) },
         where: { title: Like(`%${titleQuery}%`) },
         take: 50,
-        skip: (page - 1) * 50
+        skip: (page - 1) * 50,
       });
       if (!podcasts) {
-        return { ok: false, error: "Could not find podcasts" };
+        return { ok: false, error: 'Could not find podcasts' };
       }
       return {
         ok: true,
         podcasts,
         totalCount,
-        totalPages: Math.ceil(totalCount / 50)
+        totalPages: Math.ceil(totalCount / 50),
       };
     } catch (err) {
       console.log(err);
@@ -177,13 +178,13 @@ export class PodcastsService {
     }
     return {
       ok: true,
-      episodes: podcast.episodes
+      episodes: podcast.episodes,
     };
   }
 
   async getEpisode({
     podcastId,
-    episodeId
+    episodeId,
   }: EpisodesSearchInput): Promise<GetEpisodeOutput> {
     const { episodes, ok, error } = await this.getEpisodes(podcastId);
     if (!ok) {
@@ -193,18 +194,18 @@ export class PodcastsService {
     if (!episode) {
       return {
         ok: false,
-        error: `Episode with id ${episodeId} not found in podcast with id ${podcastId}`
+        error: `Episode with id ${episodeId} not found in podcast with id ${podcastId}`,
       };
     }
     return {
       ok: true,
-      episode
+      episode,
     };
   }
 
   async createEpisode(
     user: User,
-    { podcastId, title, category }: CreateEpisodeInput
+    { podcastId, title, category }: CreateEpisodeInput,
   ): Promise<CreateEpisodeOutput> {
     try {
       const { podcast, ok, error } = await this.getPodcast(podcastId);
@@ -212,14 +213,14 @@ export class PodcastsService {
         return { ok, error };
       }
       if (podcast.creator.id !== user.id) {
-        return { ok: false, error: "Not authorized" };
+        return { ok: false, error: 'Not authorized' };
       }
       const newEpisode = this.episodeRepository.create({ title, category });
       newEpisode.podcast = podcast;
       const { id } = await this.episodeRepository.save(newEpisode);
       return {
         ok: true,
-        id
+        id,
       };
     } catch (e) {
       return this.InternalServerErrorOutput;
@@ -228,18 +229,18 @@ export class PodcastsService {
 
   async deleteEpisode(
     user: User,
-    { podcastId, episodeId }: EpisodesSearchInput
+    { podcastId, episodeId }: EpisodesSearchInput,
   ): Promise<CoreOutput> {
     try {
       const { episode, error, ok } = await this.getEpisode({
         podcastId,
-        episodeId
+        episodeId,
       });
       if (!ok) {
         return { ok, error };
       }
       if (episode.podcast.creator.id !== user.id) {
-        return { ok: false, error: "Not authorized" };
+        return { ok: false, error: 'Not authorized' };
       }
       await this.episodeRepository.delete({ id: episode.id });
       return { ok: true };
@@ -250,18 +251,18 @@ export class PodcastsService {
 
   async updateEpisode(
     user: User,
-    { podcastId, episodeId, ...rest }: UpdateEpisodeInput
+    { podcastId, episodeId, ...rest }: UpdateEpisodeInput,
   ): Promise<CoreOutput> {
     try {
       const { episode, ok, error } = await this.getEpisode({
         podcastId,
-        episodeId
+        episodeId,
       });
       if (!ok) {
         return { ok, error };
       }
       if (episode.podcast.creator.id !== user.id) {
-        return { ok: false, error: "Not authorized" };
+        return { ok: false, error: 'Not authorized' };
       }
       const updatedEpisode = { ...episode, ...rest };
       await this.episodeRepository.save(updatedEpisode);
@@ -273,11 +274,11 @@ export class PodcastsService {
 
   async createReview(
     creator: User,
-    { title, text, podcastId }: CreateReviewInput
+    { title, text, podcastId }: CreateReviewInput,
   ): Promise<CreateReviewOutput> {
     try {
       const { ok, error: podcastFindErr, podcast } = await this.getPodcast(
-        podcastId
+        podcastId,
       );
       if (!ok || podcastFindErr) {
         return { ok: false, error: podcastFindErr };
