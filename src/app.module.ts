52eca03497723a64c06a9e1445f8c0769fa52fbf
TaehwanGo <gth1123@naver.com>
2021-01-31 17:45:48 +0900

    prod -> production

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,9 +18,9 @@ import * as Joi from 'joi';
     ConfigModule.forRoot({
       isGlobal: true, // app의 어디에서든 config module에 접근 가능한지 여부
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod', // 서버에 deploy할때 .env파일(environment variable파일)을 사용하지 않겠다는 것
+      ignoreEnvFile: process.env.NODE_ENV === 'production', // 서버에 deploy할때 .env파일(environment variable파일)을 사용하지 않겠다는 것
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(), // NODE_ENV에 test를 넣어주는 곳이 어디지?
+        NODE_ENV: Joi.string().valid('dev', 'production', 'test').required(), // NODE_ENV에 test를 넣어주는 곳이 어디지?
         DB_HOST: Joi.string().required(), // Joi.object의 key를 .env파일로 지정하고 validation조건을 입력하면 .env와 비교해서 결과를 알려주는 것 같다.
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
